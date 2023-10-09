0d3ccd9f04fbadb7fa3ee0d3372878bffe56725e
VadimNikitin1 <vchivas18years@gmail.com>
2023-10-09 14:29:41 +0400

    add theme btn

--- src/shared/Button/Button.jsx
+++ src/shared/Button/Button.jsx
@@ -1,14 +1,19 @@
+import { useSelector } from "react-redux";
+
 import clsx from "clsx";
 import style from "./Button.module.scss";
 
 const Button = ({ type, ...props }) => {
+  const theme = useSelector((state) => state.activeTab.theme);
+
   return (
     <button
       {...props}
       className={clsx(
         type === "add" && style.add,
         type === "delete" && style.delete,
-        type === "edit" && style.edit
+        type === "edit" && style.edit,
+        theme && style.light
       )}
     />
   );
