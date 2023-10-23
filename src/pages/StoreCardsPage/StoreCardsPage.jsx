d3439e98ca315de7d29091e861355143dc85d54a
VadimNikitin1 <vchivas18years@gmail.com>
2023-10-23 20:21:12 +0300

    change store page

--- src/pages/StoreCardsPage/StoreCardsPage.jsx
+++ src/pages/StoreCardsPage/StoreCardsPage.jsx
@@ -1,13 +1,27 @@
 import StoreCard from "../../entities/StoreCard/StoreCard";
-import { storeCard } from "../../assets/db";
+import Button from "../../shared/Button/Button";
+import { BsFillPlusSquareFill } from "react-icons/bs";
 import style from "./StoreCardsPage.module.scss";
 
 const StoreCardsPage = () => {
+  const storeCard = undefined;
+
   return (
-    <div className={style.StoreCardsPage}>
-      {storeCard.map((card) => (
-        <StoreCard key={card} name={card} />
-      ))}
+    <div className={style.page}>
+      <Button view={"add"}>
+        Создать магазин <BsFillPlusSquareFill />
+      </Button>
+      {storeCard === undefined || storeCard.length === 0 ? (
+        <div className={style.messageAddButton}>
+          <p className={style.message}>Нет созданных магазинов</p>
+        </div>
+      ) : (
+        <div className={style.cardList}>
+          {storeCard.map((card) => (
+            <StoreCard key={card} name={card} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
